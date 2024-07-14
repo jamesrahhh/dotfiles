58b2a8987bea423f3d2525a8623ea08710fc87b1
jamesrahhh <juntaotto777@gmail.com>
2024-07-13 20:27:24 -0500

    Upload current dotfiles

---
+++ uninstalldefaults.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Created by jamesrahhh 2024
+#
+
+sleep 1
+clear
+figlet "Uninstall Defaults"
+echo
+
+if gum confirm "DO YOU WANT TO UNINSTALL ML4W DEFAULT PROGRAMS?"; then
+  echo
+  echo ":: Uninstall started."
+elif [ $? -eq 130 ]; then
+  exit 130
+else
+  echo
+  echo ":: Uninstall cancelled."
+  exit;
+fi 
+
+yay -Rs alacritty blueman fastfetch mousepad mpv nautilus thunar thunar-archive-plugin vim xarchiver
+
+notify-send "Uninstall complete"
+echo
+echo ":: Uninstall complete."
+sleep 2
