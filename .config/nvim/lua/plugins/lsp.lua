a78353a28e133b05ce7cf7baa3759cc2822a699e
jamesrahhh <juntaotto777@gmail.com>
2024-07-13 20:28:23 -0500

    Upload Neovim dotfiles

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/neocord.lua
@@ -0,0 +1,10 @@
+return {
+  "IogaMaster/neocord",
+  lazy = false,
+  config = function()
+    require("neocord").setup {
+      main_image = "logo",
+      client_id = "1241275652405989427",
+    }
+  end,
+}
