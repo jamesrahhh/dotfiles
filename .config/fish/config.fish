c20c26e050bfb149bf01994e5bc002cc43f1a9f9
jamesrahhh <juntaotto777@gmail.com>
2024-07-19 11:21:33 -0500

    Add ML4W aliases to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,4 +14,55 @@ abbr .5 'cd ../../../../..'
 
 abbr mkdir 'mkdir -p'
 
-alias fucking="sudo"
+# -----------------------------------------------------
+# ALIASES
+# -----------------------------------------------------
+alias c='clear'
+alias nf='fastfetch'
+alias pf='fastfetch'
+alias ff='fastfetch'
+alias ls='eza -a --icons'
+alias ll='eza -al --icons'
+alias lt='eza -a --tree --level=1 --icons'
+alias shutdown='systemctl poweroff'
+alias ts='~/dotfiles/scripts/snapshot.sh'
+alias wifi='nmtui'
+alias dot="cd ~/dotfiles"
+alias cleanup='~/dotfiles/scripts/cleanup.sh'
+alias fucking='sudo'
+
+# -----------------------------------------------------
+# ML4W Apps
+# -----------------------------------------------------
+alias ml4w='~/dotfiles/apps/ML4W_Welcome-x86_64.AppImage'
+alias ml4w-settings='~/dotfiles/apps/ML4W_Dotfiles_Settings-x86_64.AppImage'
+alias ml4w-sidebar='~/dotfiles/eww/ml4w-sidebar/launch.sh'
+alias ml4w-hyprland='~/dotfiles/apps/ML4W_Hyprland_Settings-x86_64.AppImage'
+alias ml4w-diagnosis='~/dotfiles/hypr/scripts/diagnosis.sh'
+alias ml4w-hyprland-diagnosis='~/dotfiles/hypr/scripts/diagnosis.sh'
+alias ml4w-qtile-diagnosis='~/dotfiles/qtile/scripts/diagnosis.sh'
+alias ml4w-update='~/dotfiles/update.sh'
+
+# -----------------------------------------------------
+# GIT
+# -----------------------------------------------------
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gpl="git pull"
+alias gst="git stash"
+alias gsp="git stash; git pull"
+alias gcheck="git checkout"
+alias gcredential="git config credential.helper store"
+
+# -----------------------------------------------------
+# SCRIPTS
+# -----------------------------------------------------
+alias ascii='~/dotfiles/scripts/figlet.sh'
+
+# -----------------------------------------------------
+# SYSTEM
+# -----------------------------------------------------
+alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+
