6cada7602e82341f7d002860ff72f8dc58c53fa7
jamesrahhh <juntaotto777@gmail.com>
2024-09-09 04:25:49 -0500

    nvim: Add clangd to lsp config.

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,12 @@
 return {
 	{
+		"williamboman/mason.nvim",
+	},
+	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			require("nvchad.configs.lspconfig").defaults()
 			require("configs.lspconfig")
 		end,
-	},
-
-	{
-		"williamboman/mason.nvim",
 	},
 }
