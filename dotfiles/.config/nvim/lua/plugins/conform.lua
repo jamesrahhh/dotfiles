d98ae757679b4e24b8c04594a3b34f07c481cac0
jamesrahhh <juntaotto777@gmail.com>
2024-09-23 06:16:45 -0500

    nvim: Update lsp configs.

--- dotfiles/.config/nvim/lua/plugins/conform.lua
+++ dotfiles/.config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,11 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" }
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			cpp = { "clang-format" },
+			markdown = { "markdownlint" },
+		},
+	},
 }
